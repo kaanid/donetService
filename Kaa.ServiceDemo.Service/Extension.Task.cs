e4941fc5758daafa72f73d8608fc47a59172ade9
wukaiyi <wukaiyi@sodao.com>
2018-08-24 17:56:13 +0800

    新增扩展方法

---
+++ Kaa.ServiceDemo.Service/Extension.Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kaa.ServiceDemo.Service
+{
+    public static partial class Extension
+    {
+        public static Task<T> TaskEx<T>(this Task<T> task, ILogger logger, string logMsg)
+        {
+            return task.ContinueWith(c =>
+            {
+                if (!c.IsFaulted)
+                    return c.Result;
+
+                if (isLog)
+                {
+                    c.Exception.InnerExceptions.ToList().ForEach(ex =>
+                    {
+                        logger.LogError(logMsg ?? string.Empty, ex);
+                    });
+                }
+                return default(T);
+            });
+        }
+    }
+}
